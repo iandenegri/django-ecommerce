45da85256634b50b69a3620ed80afe92aef56048
iandenegri <iandenegri@hotmail.com>
2021-02-08 22:51:11 -0500

    Start adding order sum page

--- core/urls.py
+++ core/urls.py
@@ -3,14 +3,15 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-from .views import checkout, HomeView, ItemDetailView, add_to_cart, delete_from_cart
+from .views import checkout, HomeView, ItemDetailView, OrderSummaryView, add_to_cart, delete_from_cart
 
 app_name = 'core'
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('item/<slug>', ItemDetailView.as_view(), name='item_detail'),
     path('checkout/', checkout, name='checkout'),
+    path('order-summary/', OrderSummaryView.as_view(), name='order_summary'),
+    path('item/<slug>', ItemDetailView.as_view(), name='item_detail'),
     path('add-to-cart/<slug>', add_to_cart, name='add_to_cart'),
     path('delete-from-cart/<slug>', delete_from_cart, name='delete_from_cart')
 ]
--- core/views.py
+++ core/views.py
@@ -1,20 +1,26 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, View
 from django.utils import timezone
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Local
 from .models import Item, Order, OrderItem
 
+
 # Create your views here.
+# CBV's
 class HomeView(ListView):
     model = Item
     paginate_by = 10
     template_name = 'home.html'
 
+
 class ItemDetailView(DetailView):
     model = Item
     template_name = 'item.html'
+
 
 def checkout(request):
     context = {
@@ -22,6 +28,22 @@ def checkout(request):
     }
     return render(request, "checkout.html", context)
 
+
+class OrderSummaryView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        try:
+            order = Order.objects.get(user=self.request.user, ordered=False)
+            context = {
+                'object': order 
+            }
+            return render(self.request, 'order_summary.html', context=context)
+        except Order.DoesNotExist:
+            messages.error(self.request, "You don't have an active order.")
+            return redirect("/")
+
+
+# FBV's
+@login_required
 def add_to_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     order, created = Order.objects.get_or_create(user=request.user, ordered=False) # The user should only have one on going order that is False. If they don't have one at all, we should create one, right?
@@ -44,6 +66,7 @@ def add_to_cart(request, slug):
             order.items.add(order_item)
     return redirect('core:item_detail', slug = slug)
 
+@login_required
 def delete_from_cart(request, slug):
     item = get_object_or_404(Item, slug=slug)
     try:
